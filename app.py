2225877fe27bfe9616a90ce649435c0edc338e1b
Iliass <tiiliass@gmail.com>
2015-09-10 01:08:32 -0700

    added comments

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 #!flask/bin/python
 from flask import Flask, jsonify
+from flask import abort
 
 app = Flask(__name__)
 
@@ -18,9 +19,20 @@ tasks = [
     }
 ]
 
+# get the entire database
 @app.route('/todo/api/v1.0/tasks', methods=['GET'])
 def get_tasks():
     return jsonify({'tasks':tasks})
+
+# get a task by id
+@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['GET'])
+def get_task(task_id):
+    task = [task for task in tasks if task['id'] == task_id]
+    if len(task) == 0:
+        abort(404)
+    return jsonify({'tasks':task[0]})
+   
+# nice error handler
 
 if __name__ == '__main__':
     app.run(debug=True)
