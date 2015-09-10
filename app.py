e2c591c3e51eefebe4a7944032aad8b54dc5fd56
Iliass <tiiliass@gmail.com>
2015-09-10 01:15:36 -0700

    added error handler

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 #!flask/bin/python
 from flask import Flask, jsonify
 from flask import abort
+from flask import make_response
 
 app = Flask(__name__)
 
@@ -33,6 +34,9 @@ def get_task(task_id):
     return jsonify({'tasks':task[0]})
    
 # nice error handler
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 if __name__ == '__main__':
     app.run(debug=True)
